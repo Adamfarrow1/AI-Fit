367e472c9599cdb136c5e3be4f4a156fb5effd58
Adamfarrow1 <Adamfarrow1@gmail.com>
2023-10-07 19:10:06 -0400

    styles

--- components/Login.js
+++ components/Login.js
@@ -1,29 +1,38 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet, Image } from 'react-native';
-import { useNavigation } from '@react-navigation/native'; 
+import { View, TextInput, Button, StyleSheet, Image, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import axios from 'axios';
+import TypeWriter from 'react-native-typewriter';
+
 export default function Login() {
-    const navigation = useNavigation(); // Use the useNavigation hook
+  const navigation = useNavigation();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [type, setType] = useState(true);
 
-  const  handleLogin = async () => {
+  const handleLogin = async () => {
     try {
       const response = await axios.post('http://10.127.130.59:3000/login', {
-        name: username, // Updated state variable name
+        name: username,
         password: password,
       });
 
       if (response.data.message === 'Login successful') {
         console.log('User login successful');
-        // Optionally, you can navigate to another screen after successful registration
+        setUsername('');
+        setPassword('');
+        // Optionally, you can navigate to another screen after successful login
         // navigation.navigate('SuccessScreen');
       }
     } catch (error) {
       console.error('Error login user:', error);
     }
-    setUsername('');
-      setPassword('');
+  };
+
+  const handleTypewriter = () => {
+    setTimeout(() => {
+      setType((prevType) => !prevType);
+    }, 2500); // Delay in milliseconds (adjust as needed)
   };
 
   React.useLayoutEffect(() => {
@@ -32,45 +41,52 @@ export default function Login() {
     });
   }, [navigation]);
 
-
-
   return (
     <View style={styles.container}>
-      <Image
-        source={require('../assets/fitness-sport-gym-logo-design-vector.jpg')}
-        style={styles.logo}
-      />
-      <View style={styles.center}>
-        <TextInput
-          style={styles.input}
-          placeholderTextColor="#6b6776"
-          placeholder="Username"
-          value={username}
-          onChangeText={(text) => setUsername(text)}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          secureTextEntry={true}
-          placeholderTextColor="#6b6776"
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-        />
-        <View style={styles.apart}>
-          <View style={styles.button}>
-              <Button
-                title="Login"
-                onPress={handleLogin}
-                color="black"
-              />
-          </View>
+      <View style={styles.spacing}>
+        <View style={styles.helloWorldContainer}>
+          {type ? (
+            <Text style={styles.textcolor}>
+              <TypeWriter onTypingEnd={handleTypewriter} minDelay={150} typing={1}>
+                Welcome to Fit AI
+              </TypeWriter>
+            </Text>
+          ) : (
+            <Text style={styles.textcolor}>
+              <TypeWriter onTypingEnd={handleTypewriter} minDelay={150} typing={1}>
+                Become Fit Today.
+              </TypeWriter>
+            </Text>
+          )}
+        </View>
 
-          
+        <View style={styles.center}>
+          <TextInput
+            style={styles.input}
+            placeholderTextColor="#6b6776"
+            placeholder="Username"
+            value={username}
+            onChangeText={(text) => setUsername(text)}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            secureTextEntry={true}
+            placeholderTextColor="#6b6776"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+          />
+          <View style={styles.apart}>
+            <View style={styles.button}>
+              <Button title="Login" onPress={handleLogin} color="black" />
+            </View>
+
             <Button
               title="New User? Register"
-              onPress={() => navigation.navigate('Register')} // Use the navigation object
+              onPress={() => navigation.navigate('Register')}
               color="#6b6776"
             />
+          </View>
         </View>
       </View>
     </View>
@@ -92,21 +108,15 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     borderRadius: 12,
     paddingLeft: 10,
-    color: 'black', // Text color for input fields
-  },
-  logo: {
-    width: 150,
-    height: 150,
-    borderRadius: 50,
-    marginBottom: 100,
+    color: 'black',
   },
   button: {
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: "aqua",
+    backgroundColor: 'aqua',
     borderColor: 'grey',
     height: 55,
-    width:250,
+    width: 250,
     borderRadius: 20,
     marginTop: 15,
   },
@@ -117,6 +127,17 @@ const styles = StyleSheet.create({
   apart: {
     justifyContent: 'space-between',
     alignItems: 'center',
-  }
-
+  },
+  textcolor: {
+    color: 'aqua',
+    fontSize: 30,
+  },
+  spacing: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  helloWorldContainer: {
+    marginBottom: 130,
+    height: 50 // Adjust the margin as needed
+  },
 });
