2ed4b3766571984df3427d31fc221898af9553bf
Colton Curtis <68677235+ColtonsProjects@users.noreply.github.com>
2023-10-07 19:20:22 -0400

    Update Register.js
    cleaaannnnnn

--- components/Register.js
+++ components/Register.js
@@ -1,76 +1,100 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet, Image } from 'react-native';
+import {
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import axios from 'axios';
 
 export default function Register() {
-  const [name, setName] = useState(''); // Change the state variable name
-  const [password, setPassword] = useState('');
+  const [step, setStep] = useState(1); // To keep track of the current step
+  const [fullName, setFullName] = useState('');
   const [email, setEmail] = useState('');
+  const [age, setAge] = useState('');
+  const [gender, setGender] = useState('');
+  const [weight, setWeight] = useState('');
+  const [height, setHeight] = useState('');
+  const [goal, setGoal] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   const navigation = useNavigation();
 
-  const handleRegister = async () => {
-    try {
-      const response = await axios.post('http://10.127.130.59:3000/registerUser', {
-        name: name, // Updated state variable name
-        password: password,
-        email: email,
-      });
+  const dismissKeyboard = () => {
+    Keyboard.dismiss(); // Dismiss the keyboard when the screen is tapped
+  };
 
-      if (response.data.message === 'User registered successfully') {
-        console.log('User registered successfully');
-        // Optionally, you can navigate to another screen after successful registration
-        // navigation.navigate('SuccessScreen');
-      }
-    } catch (error) {
-      console.error('Error registering user:', error);
+  const advanceStep = () => {
+    if (step < 9) {
+      setStep(step + 1);
+    } else {
+      console.log('Full Registration Details:', {
+        fullName, email, age, gender, weight, height, goal, username, password
+      });
+      // Implement additional registration logic here if needed
+    }
+  };
+
+  const getQuestionText = () => {
+    switch(step) {
+      case 1: return "Full Name";
+      case 2: return "Email";
+      case 3: return "Age";
+      case 4: return "Gender (e.g. Male, Female)";
+      case 5: return "Weight";
+      case 6: return "Height";
+      case 7: return "Fitness Goal (e.g. Weight Loss, Muscle Gain)";
+      case 8: return "Username";
+      case 9: return "Password";
+      default: return "";
+    }
+  };
+
+  const renderInput = () => {
+    switch(step) {
+      case 1: return <TextInput style={styles.input} placeholder="Full Name" onChangeText={(text) => setFullName(text)} />;
+      case 2: return <TextInput style={styles.input} placeholder="Email" onChangeText={(text) => setEmail(text)} />;
+      case 3: return <TextInput style={styles.input} placeholder="Age" keyboardType="numeric" onChangeText={(text) => setAge(text)} />;
+      case 4: return <TextInput style={styles.input} placeholder="Gender (e.g. Male, Female)" onChangeText={(text) => setGender(text)} />;
+      case 5: return <TextInput style={styles.input} placeholder="Weight" keyboardType="numeric" onChangeText={(text) => setWeight(text)} />;
+      case 6: return <TextInput style={styles.input} placeholder="Height" keyboardType="numeric" onChangeText={(text) => setHeight(text)} />;
+      case 7: return <TextInput style={styles.input} placeholder="Fitness Goal (e.g. Weight Loss, Muscle Gain)" onChangeText={(text) => setGoal(text)} />;
+      case 8: return <TextInput style={styles.input} placeholder="Username" onChangeText={(text) => setUsername(text)} />;
+      case 9: return <TextInput style={styles.input} placeholder="Password" secureTextEntry={true} onChangeText={(text) => setPassword(text)} />;
+      default: return null;
     }
   };
 
   React.useLayoutEffect(() => {
     navigation.setOptions({
       title: 'Register',
-      headerStyle: {
-        backgroundColor: 'white',
-      },
-      headerTintColor: 'black',
-      headerTitleStyle: {
-        fontWeight: 'bold',
-      },
-      headerBackTitle: ' ',
+      // ... other options ...
     });
   }, [navigation]);
 
   return (
-    <View style={styles.container}>
-      <Image
-        source={require('../assets/fitness-sport-gym-logo-design-vector.jpg')}
-        style={styles.logo}
-      />
-      <View>
-        <TextInput
-          style={styles.input}
-          placeholderTextColor="black"
-          placeholder="Email"
-          onChangeText={(text) => setEmail(text)}
-        />
-        <TextInput
-          style={styles.input}
-          placeholderTextColor="black"
-          placeholder="Username"
-          onChangeText={(text) => setName(text)} // Updated state variable name
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          secureTextEntry={true}
-          placeholderTextColor="black"
-          onChangeText={(text) => setPassword(text)}
-        />
-
-        <Button title="Register" onPress={handleRegister} color="black" />
-      </View>
-    </View>
+    <TouchableWithoutFeedback onPress={dismissKeyboard}>
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      >
+        <View style={styles.spacing}>
+          <Text style={styles.textcolor}>{getQuestionText()}</Text>
+          <View style={styles.center}>
+            {renderInput()}
+            <View style={styles.button}>
+              <Button title="Next" onPress={advanceStep} color="black" />
+            </View>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -79,7 +103,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'white',
+    backgroundColor: '#201a30',
   },
   input: {
     width: 300,
@@ -91,10 +115,31 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
     color: 'black',
   },
-  logo: {
-    width: 150,
-    height: 150,
-    borderRadius: 50,
-    marginBottom: 100,
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'aqua',
+    borderColor: 'grey',
+    height: 55,
+    width: 250,
+    borderRadius: 20,
+    marginTop: 15,
+  },
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  apart: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  textcolor: {
+    color: 'aqua',
+    fontSize: 30,
+    marginBottom: 30,
+  },
+  spacing: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
 });
