ec338ad456f255ccbcbd3aca1927d67d2145971a
Colton Curtis <coltoncurtis@Coltons-Air.attlocal.net>
2023-10-09 08:17:02 -0400

    NEW COMMIT ATTEMPT

---
+++ components/Login.js
@@ -0,0 +1,155 @@
+import React, { useState } from 'react';
+import {
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  Image,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import axios from 'axios';
+import TypeWriter from 'react-native-typewriter';
+
+export default function Login() {
+  const navigation = useNavigation();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [type, setType] = useState(true);
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://10.127.130.59:3000/login', {
+        name: username,
+        password: password,
+      });
+
+      if (response.data.message === 'Login successful') {
+        console.log('User login successful');
+        setUsername('');
+        setPassword('');
+        // Optionally, you can navigate to another screen after successful login
+        // navigation.navigate('SuccessScreen');
+      }
+    } catch (error) {
+      console.error('Error login user:', error);
+    }
+  };
+
+  const handleTypewriter = () => {
+    setTimeout(() => {
+      setType((prevType) => !prevType);
+    }, 2500); // Delay in milliseconds (adjust as needed)
+  };
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false, // Hide the navigation header
+    });
+  }, [navigation]);
+
+  const dismissKeyboard = () => {
+    Keyboard.dismiss(); // Dismiss the keyboard when the screen is tapped
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={dismissKeyboard}>
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      >
+        <View style={styles.spacing}>
+          <View style={styles.helloWorldContainer}>
+            {type ? (
+              <TypeWriter fixed={true} onTypingEnd={handleTypewriter} style={styles.textcolor} minDelay={120} typing={1}>
+                Welcome to Fit AI
+              </TypeWriter>
+            ) : (
+              <TypeWriter fixed={true} onTypingEnd={handleTypewriter} style={styles.textcolor} minDelay={120} typing={1}>
+                Become Fit Today.
+              </TypeWriter>
+            )}
+          </View>
+
+          <View style={styles.center}>
+            <TextInput
+              style={styles.input}
+              placeholderTextColor="#6b6776"
+              placeholder="Username"
+              value={username}
+              onChangeText={(text) => setUsername(text)}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              secureTextEntry={true}
+              placeholderTextColor="#6b6776"
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+            />
+            <View style={styles.apart}>
+              <View style={styles.button}>
+                <Button title="Login" onPress={handleLogin} color="black" />
+              </View>
+
+              <Button title="New User? Register" onPress={() => navigation.navigate('Register')} color="#6b6776" />
+            </View>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#201a30',
+  },
+  input: {
+    width: 300,
+    height: 50,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    borderRadius: 12,
+    paddingLeft: 10,
+    color: 'white',
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'aqua',
+    borderColor: 'grey',
+    height: 55,
+    width: 250,
+    borderRadius: 20,
+    marginTop: 15,
+  },
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  apart: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  textcolor: {
+    color: 'aqua',
+    fontSize: 30,
+  },
+  spacing: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  helloWorldContainer: {
+    marginBottom: 130,
+    height: 50,
+  },
+});
---
+++ components/Register.js
@@ -0,0 +1,267 @@
+import React, { useState } from 'react';
+import {
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Picker } from '@react-native-picker/picker';
+import axios from 'axios';
+
+export default function Register() {
+  const [step, setStep] = useState(1); // To keep track of the current step
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [age, setAge] = useState('');
+  const [gender, setGender] = useState('');
+  const [weight, setWeight] = useState('');
+  const [heightFeet, setHeightFeet] = useState('5');  // Defaulting to 5 feet
+  const [heightInches, setHeightInches] = useState(0); // Defaulting to 0 inches
+  const [goal, setGoal] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const height = `${heightFeet}'${heightInches}"`;
+  const navigation = useNavigation();
+
+
+
+  const handleRegister = async () => {
+    try {
+      console.log('Attempting to create user');
+      const height = `${heightFeet}'${heightInches}"`;
+      const response = await axios.post('http://192.168.1.178:3000/registerUser', {
+        name: username,
+        password: password,
+        fullName: fullName,
+        email: email,
+        age: age,
+        gender: gender,
+        weight: weight,
+        height: height,
+        goal: goal,
+      });
+
+      console.log('Step one done');
+
+
+      if (response.data.message === 'User registered successfully') {
+        console.log('User register successful');
+        setUsername('');
+        setPassword('');
+        // Optionally, you can navigate to another screen after successful login
+        // navigation.navigate('SuccessScreen');
+      }
+    } catch (error) {
+      console.error('Error registering user:', error.message);
+      console.error('Stack trace:', error.stack);
+    }
+  };
+
+
+
+
+  const dismissKeyboard = () => {
+    Keyboard.dismiss(); // Dismiss the keyboard when the screen is tapped
+  };
+
+  const advanceStep = () => {
+    if (step < 9) {
+      setStep(step + 1);
+    } else {
+      handleRegister();
+      console.log('Full Registration Details:', {
+        fullName, email, age, gender, weight, height, goal, username, password
+      });
+      // Implement additional registration logic here if needed
+    }
+  };
+
+  const getQuestionText = () => {
+    switch(step) {
+      case 1: return "Full Name";
+      case 2: return "Email";
+      case 3: return "Age";
+      case 4: return "Gender (e.g. Male, Female)";
+      case 5: return "Weight";
+      case 6: return "Height";
+      case 7: return "Fitness Goal (e.g. Weight Loss, Muscle Gain)";
+      case 8: return "Username";
+      case 9: return "Password";
+      default: return "";
+    }
+  };
+
+  const renderInput = () => {
+    switch(step) {
+      case 1: return <TextInput value={fullName} style={styles.input} placeholder="Full Name" onChangeText={(text) => setFullName(text)} />;
+      case 2: return <TextInput value={email} style={styles.input} placeholder="Email" onChangeText={(text) => setEmail(text)} />;
+      case 3: // Age
+        return (
+          <Picker
+            selectedValue={age}
+            style={styles.picker}
+            itemStyle={{ color: 'white' }}
+            onValueChange={(itemValue) => setAge(itemValue)}
+          >
+            {[...Array(100).keys()].map((_, index) => (
+              <Picker.Item key={index} label={String(index + 1)} value={String(index + 1)} />
+            ))}
+          </Picker>
+        );
+      case 4: return <TextInput value={gender} style={styles.input} placeholder="Gender (e.g. Male, Female)" onChangeText={(text) => setGender(text)} />;
+      case 5: // Weight
+        return (
+          <Picker
+              selectedValue={age}
+              style={styles.picker}
+              itemStyle={{ color: 'white' }}
+              onValueChange={(itemValue) => setAge(itemValue)}
+          >
+              {[...Array(341).keys()].map((_, index) => (
+                  <Picker.Item key={index} label={String(index + 60)} value={String(index + 60)} />
+              ))}
+          </Picker>
+        );
+        case 6:
+          return (
+            <View style={styles.heightContainer}>
+              <Picker
+                selectedValue={heightFeet}
+                style={styles.heightPicker}
+                itemStyle={{ color: 'white' }}
+                onValueChange={(itemValue) => setHeightFeet(itemValue)}
+              >
+                {[...Array(8).keys()].map((_, index) => (
+                  <Picker.Item key={index} label={String(index + 3)} value={String(index + 3)} /> 
+                ))}
+              </Picker>
+              <Text style={styles.heightText}>ft</Text>
+              <Picker
+                selectedValue={heightInches}
+                style={styles.heightPicker}
+                itemStyle={{ color: 'white' }}
+                onValueChange={(itemValue) => setHeightInches(itemValue)}
+              >
+                {[...Array(12).keys()].map((_, index) => (
+                  <Picker.Item key={index} label={String(index)} value={index} />
+                ))}
+              </Picker>
+              <Text style={styles.heightText}>in</Text>
+            </View>
+          );
+        
+      case 7: return <TextInput value={goal} style={styles.input} placeholder="Fitness Goal (e.g. Weight Loss, Muscle Gain)" onChangeText={(text) => setGoal(text)} />;
+      case 8: return <TextInput value={username} style={styles.input} placeholder="Username" onChangeText={(text) => setUsername(text)} />;
+      case 9: return <TextInput value={password} style={styles.input} placeholder="Password" secureTextEntry={true} onChangeText={(text) => setPassword(text)} />;
+      default: return null;
+    }
+  };
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      title: 'Register',
+      // ... other options ...
+    });
+  }, [navigation]);
+
+  return (
+    <TouchableWithoutFeedback onPress={dismissKeyboard}>
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      >
+        <View style={styles.spacing}>
+          <Text style={styles.textcolor}>{getQuestionText()}</Text>
+          <View style={styles.center}>
+            {renderInput()}
+            <View style={styles.button}>
+              <Button title="Next" onPress={advanceStep} color="black" />
+            </View>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#201a30',
+  },
+
+
+  heightContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: 300,
+  },
+  
+  heightPicker: {
+    width: 100,
+    height: 220,
+    color: 'white',
+  },
+  
+  heightText: {
+    color: 'white',
+    fontSize: 18,
+    marginHorizontal: 5,
+  },
+  
+  input: {
+    width: 300,
+    height: 70,
+    borderColor: 'dimgray',
+    borderWidth: 1,
+    marginBottom: 20,
+    borderRadius: 15,  // Increased for a softer edge
+    paddingLeft: 15,   // Slight increase for better spacing
+    paddingTop: 10,    // Vertical padding for better spacing
+    paddingBottom: 10,
+    fontSize: 18,      // Increased font size
+    color: 'white',
+    textAlign: 'center', // Center the text
+    elevation: 5,
+},
+  picker: {  
+    width: 300,
+    height: 200,
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'aqua',
+    borderColor: 'grey',
+    height: 55,
+    width: 250,
+    borderRadius: 20,
+    marginTop: 15,
+  },
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  apart: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  textcolor: {
+    color: 'aqua',
+    fontSize: 30,
+    marginBottom: 30,
+  },
+  spacing: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+});
